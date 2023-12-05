e7f45281da2ea8ab0d428f95686fc865246dc09f
ibrahim77gh <syedibrahim4091@gmail.com>
2023-12-06 03:02:26 +0500

    Commit

--- script.js
+++ script.js
@@ -1,62 +1,176 @@
-const sidebar = document.querySelector("#sidebar");
-const hide_sidebar = document.querySelector(".hide-sidebar");
-const new_chat_button = document.querySelector(".new-chat");
+document.addEventListener("DOMContentLoaded", function () {
+    const newChatView = document.querySelector(".new-chat-view");
+    const conversationView = document.querySelector(".conversation-view");
+    const messageForm = document.getElementById("message-form");
+    const messageInput = document.getElementById("message");
+    const sendButton = document.querySelector(".send-button");
+    const newChatButton = document.querySelector(".new-chat");
+    const conversationContainer = document.querySelector(".conversation-view");
 
-hide_sidebar.addEventListener( "click", function() {
-    sidebar.classList.toggle( "hidden" );
-} );
+    // Display the new-chat-view initially
+    conversationView.style.display = "none";
 
-const user_menu = document.querySelector(".user-menu ul");
-const show_user_menu = document.querySelector(".user-menu button");
+    let currentThreadId;
 
-show_user_menu.addEventListener( "click", function() {
-    if( user_menu.classList.contains("show") ) {
-        user_menu.classList.toggle( "show" );
-        setTimeout( function() {
-            user_menu.classList.toggle( "show-animate" );
-        }, 200 );
-    } else {
-        user_menu.classList.toggle( "show-animate" );
-        setTimeout( function() {
-            user_menu.classList.toggle( "show" );
-        }, 50 );
+    // Function to disable the send button
+    function disableSendButton() {
+        sendButton.disabled = true;
     }
-} );
 
-const models = document.querySelectorAll(".model-selector button");
-
-for( const model of models ) {
-    model.addEventListener("click", function() {
-        document.querySelector(".model-selector button.selected")?.classList.remove("selected");
-        model.classList.add("selected");
-    });
-}
-
-const message_box = document.querySelector("#message");
-
-message_box.addEventListener("keyup", function() {
-    message_box.style.height = "auto";
-    let height = message_box.scrollHeight + 2;
-    if( height > 200 ) {
-        height = 200;
+    // Function to enable the send button
+    function enableSendButton() {
+        sendButton.disabled = false;
     }
-    message_box.style.height = height + "px";
-});
 
-function show_view( view_selector ) {
-    document.querySelectorAll(".view").forEach(view => {
-        view.style.display = "none";
+    // Function to scroll the conversation view to the bottom
+    function scrollConversationToBottom() {
+        conversationContainer.scrollTop = conversationContainer.scrollHeight;
+    }
+
+    // Function to show loading indicator
+    function showLoadingIndicator() {
+        const loadingDiv = document.getElementById("loading");
+        loadingDiv.style.display = "block";
+        conversationContainer.appendChild(loadingDiv); // Move loading div after the user message
+        disableSendButton();
+    }
+    
+    // Function to hide loading indicator
+    function hideLoadingIndicator() {
+        const loadingDiv = document.getElementById("loading");
+        loadingDiv.style.display = "none";
+        enableSendButton()
+    }
+
+    // Make a GET request to the "http://127.0.0.1:5000/start" endpoint to create a new thread when the page is loaded
+    fetch("http://127.0.0.1:5000/start")
+        .then(response => response.json())
+        .then(data => {
+            currentThreadId = data.thread_id;
+        })
+        .catch(error => {
+            console.error("Error starting a new conversation:", error);
+        });
+
+    // Function to switch from new-chat-view to conversation-view
+    function switchToConversationView() {
+        newChatView.style.display = "none";
+        conversationView.style.display = "block";
+    }
+
+    // Function to handle user messages
+    function handleUserMessage() {
+        const userMessage = messageInput.value.trim();
+        if (userMessage !== "") {
+            // Create and append the user message div
+            appendUserMessage(userMessage);
+
+            // Call function for AI reply
+            handleAIReply(userMessage);
+
+            // Clear the message input
+            messageInput.value = "";
+
+            switchToConversationView();
+            scrollConversationToBottom();
+        }
+    }
+
+    // Function to append a user message to the conversation view
+    function appendUserMessage(message) {
+        const userMessageDiv = createUserMessageDiv(message);
+        conversationContainer.appendChild(userMessageDiv);
+    }
+
+    // Function to create a user message div
+    function createUserMessageDiv(message) {
+        const userMessageDiv = document.createElement("div");
+        userMessageDiv.classList.add("user", "message");
+        userMessageDiv.innerHTML = `
+            <div class="identity">
+                <i class="user-icon">u</i>
+            </div>
+            <div class="content">
+                <p>${message}</p>
+            </div>
+        `;
+        return userMessageDiv;
+    }
+
+    // Function to handle AI replies
+    function handleAIReply(userQuery) {
+        if (!currentThreadId) {
+            console.error("Error: Thread ID is not available.");
+            return;
+        }
+
+        showLoadingIndicator();
+
+        // Make a POST request to the "http://127.0.0.1:5000/chat" endpoint with the thread_id and message
+        fetch("http://127.0.0.1:5000/chat", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                thread_id: currentThreadId,
+                message: userQuery,
+            }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                const aiResponse = data.response;
+
+                // Append the AI response to the conversation view
+                appendAssistantMessage(aiResponse);
+                hideLoadingIndicator();
+                scrollConversationToBottom();
+            })
+            .catch(error => {
+                console.error("Error fetching AI response:", error);
+            });
+    }
+
+    // Function to append an assistant message to the conversation view
+    function appendAssistantMessage(message) {
+        const assistantMessageDiv = createAssistantMessageDiv(message);
+        conversationContainer.appendChild(assistantMessageDiv);
+    }
+
+    // Function to create an assistant message div
+    function createAssistantMessageDiv(message) {
+        const assistantMessageDiv = document.createElement("div");
+        assistantMessageDiv.classList.add("assistant", "message");
+        assistantMessageDiv.innerHTML = `
+            <div class="identity">
+                <img src="lily-chat-icon.png" alt="Girl in a jacket" width="35" height="35">
+            </div>
+            <div class="content">
+                <p>${message}</p>
+            </div>
+        `;
+        return assistantMessageDiv;
+    }
+
+    // Event listener for the send button
+    sendButton.addEventListener("click", handleUserMessage);
+
+    newChatButton.addEventListener("click", function () {
+        // Make a GET request to the "http://127.0.0.1:5000/start" endpoint to get the thread_id
+        fetch("http://127.0.0.1:5000/start")
+            .then(response => response.json())
+            .then(data => {
+                currentThreadId = data.thread_id;
+
+                // Clear the conversation view
+                conversationContainer.innerHTML = "";
+
+                // Display the new-chat-view again
+                newChatView.style.display = "block";
+                conversationView.style.display = "none";
+            })
+            .catch(error => {
+                console.error("Error starting a new conversation:", error);
+            });
     });
-
-    document.querySelector(view_selector).style.display = "flex";
-}
-
-new_chat_button.addEventListener("click", function() {
-    show_view( ".new-chat-view" );
-});
-
-document.querySelectorAll(".conversation-button").forEach(button => {
-    button.addEventListener("click", function() {
-        show_view( ".conversation-view" );
-    })
 });
